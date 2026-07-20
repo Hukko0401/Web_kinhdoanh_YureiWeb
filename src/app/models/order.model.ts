d2c89bff2837f25940676216faa51c6110a132c4
Hukkonegit config --global --list <khoanh23416@st.uel.edu.vn>
2026-07-20 20:23:04 +0700

    Fix orderHistory badge

--- src/app/models/order.model.ts
+++ src/app/models/order.model.ts
@@ -1,10 +1,10 @@
 export interface Order {
-order_id: string
-user_id: string
-status: 'processing' | 'shipping' | 'completed' | 'cancelled'
-receiver_name: string
-receiver_phone: string
-address: string
-shipping_fee: number
-created_at: string
+  order_id: string
+  user_id: string
+  status: 'pending' | 'payment_failed' | 'processing' | 'shipping' | 'completed' | 'cancelled'
+  receiver_name: string
+  receiver_phone: string
+  address: string
+  shipping_fee: number
+  created_at: string
 }
